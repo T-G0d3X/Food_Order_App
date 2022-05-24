03b4ee5a544d8488fedb25e67a3ed0412ae0937f
Tihomir Godek <tihomirgodek24@gmail.com>
2022-05-24 11:37:15 +0200

    Adding components and styles
    Adding state to open and close modal

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,6 @@
 // render a single meal item
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
@@ -8,9 +9,12 @@ const MealItem = (props) => {
     <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
       </div>
-      <div className={classes.description}>{props.description}</div>
-      <div className={classes.price}>{price}</div>
+      <div>
+        <MealItemForm id={props.id} />
+      </div>
     </li>
   );
 };
